2989598831e98744c1508eba4be3c3a48e7ccf4e
Yudi Candra <yudhichandra02@gmail.com>
2022-08-27 00:29:06 +0700

    update title

--- resources/views/livewire/guest/contact-us.blade.php
+++ resources/views/livewire/guest/contact-us.blade.php
@@ -1,7 +1,9 @@
 <div class="mx-12 my-12 flex justify-between">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.063983378449!2d106.76583346605233!3d-6.2553013270995015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f161b9a05c69%3A0xb2b59baa960b478b!2sJasaklan!5e0!3m2!1sid!2sid!4v1661533628590!5m2!1sid!2sid" width="600"
-        height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <ul class="w-1/2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 text-white pr-4 shadow-0">
+    <div class="max-w-1/2">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.063983378449!2d106.76583346605233!3d-6.2553013270995015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f161b9a05c69%3A0xb2b59baa960b478b!2sJasaklan!5e0!3m2!1sid!2sid!4v1661533628590!5m2!1sid!2sid" width="450"
+            height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <ul class="w-1/2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700  pr-4 shadow-0">
         <li class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600 flex justify-between align-items-center">
             <span><strong>Email</strong></span>
         </li>
