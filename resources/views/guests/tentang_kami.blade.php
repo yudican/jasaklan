eb58184d830bd6d06bcd3f37fe2b8941b7357bd7
Yudi Candra <yudhichandra02@gmail.com>
2022-08-27 11:04:51 +0700

    update url

--- resources/views/guests/tentang_kami.blade.php
+++ resources/views/guests/tentang_kami.blade.php
@@ -15,26 +15,113 @@
                 <h2 class="font-bold text-3xl md:text-4 lg:text-3xl text-center mb-6">
                     Tentang Kami
                 </h2>
-                <p class=" lg:text-md text-center m-6 md:text-xs">
-                    merupakan salah satu situs yang membantu para pembisnis, blogger dan juga youtuber dalam
-                    meningkatkan kunjungan di blog atau youtube mereka tentunya dnegan harga yang terjangkau.
+                <p>
+                    <style type="text/css">
+                        p.p1 {
+                            margin: 5.0px 0.0px 5.0px 0.0px;
+                            font: 24px;
+                        }
 
-                    Kami menyadari bahwa kunjungan yang tinggi akan mempengaruhi penjualan produk atau jasa yang Anda
-                    tawarkan sehingga situs ini terbentuk.
+                        p.p2 {
+                            margin: 5.0px 0.0px 5.0px 0.0px;
+                            text-align: justify;
+                            font: 18px;
+                        }
 
-                    Belum lagi para youtuber yang sulit mencapai persyaratan monetize yakni 4000 jam dan 1000 Subcribe.
-                    Dengan layanan ini semua hal tersebut dipastikan akan mudah tercapai.
+                        p.p3 {
+                            margin: 5.0px 0.0px 5.0px 0.0px;
+                            text-align: justify;
+                            font: 24px;
+                        }
 
-                    jasaklan.com juga membantu siapapun yang lagi membutuhkan penghasilan tambahan, hanya dnegan melihat
-                    iklan yang dipasang oleh Advertiser, uang langsung masuk ke akun anda. Para viewer bisa memilih
-                    sendiri iklan mana yang mau dilihat karena setiap iklan akan memiliki rate harga yang berbeda beda.
+                        li.li2 {
+                            margin: 5.0px 0.0px 5.0px 0.0px;
+                            text-align: justify;
+                            font: 18px;
+                        }
 
-                    Kontak Kami :
+                        span.s1 {
+                            font: 14px Symbol
+                        }
 
-                    WA : 0895614401040
+                        span.Apple-tab-span {
+                            white-space: pre
+                        }
+
+                        ul.ul1 {
+                            list-style-type: disc
+                        }
+                    </style>
+                <p class="p1"><strong>Tentang Kami Jasaklan.com</strong></p>
+                <p class="p2">Jasaklan.com merupakan perusahaan yang bergerak di bidang jasa. Kami melayani jasa subscribe, like, comment, views, share, followers, click ads blog,<span class="Apple-converted-space">&nbsp;&nbsp;</span>dan posting URL custom. Kami senantiasa berupaya untuk
+                    meningkatkan popularitas dan memajukan bisnis online yang Anda tekuni.</p>
+                <p class="p2">Tak hanya siap melayani bidang jasa tersebut, kami juga senantiasa memberi peluang bagi siapa saja yang ingin memperoleh penghasilan. Dengan menjadi member, anda berperan menjadi viewers. Hanya dengan menonton dan mengerjakan tugas dari advertiser, anda akan mendapat
+                    komisi hingga jutaan rupiah. Syarat untuk bergabung dengan jasaklan sangatlah mudah, Anda hanya perlu akun media sosial seperti youtube, facebook, instagram, twitter, dan akun social media lainnya yang real. Syarat ini kami utamakan guna menjaga kualitas bagi advertiser, Kualitas
+                    selalu kami utamakan dengan tujuan agar tak mengecewakan Anda dan advertiser.</p>
+                <p class="p2">Kami juga memberikan peluang bagi viewers untuk menjadi advertiser. KEREN KAN ? Bisa menghasilkan cuan, sekaligus bisa buat iklan.</p>
+                <p class="p3"><strong>Sejarah Perusahaan</strong></p>
+                <p class="p2">Seiring kemajuan teknologi, penggunaan sosial media untuk memenuhi segala kebutuhan kian meningkat. Hal ini sudah menjadi sesuatu yang lumrah. Mengingat sosial media memiliki jangkauan yang luas sehingga pemanfaatannya bisa memberikan keuntungan optimal.</p>
+                <p class="p2">Menyadari akan hal tersebut, kami mengembangkan jasaklan.com untuk membantu siapa saja yang aktif di sosial media agar mendapatkan penghasilan tambahan. Jasaklan.com siap menyediakan subscribe, comment, like, share bahkan followers untuk memudahkan keperluan Anda di
+                    dunia online.</p>
+                <p class="p2">Tak terkecuali bagi Anda yang ingin menggeluti platform YouTube. Sebagaimana yang kita tahu bahwa YouTube memerlukan subscribe, viewers, like, dan lain sebagainya agar bisa berkembang dengan baik.</p>
+                <p class="p2">Pada dasarnya, semakin banyak subscribe maupun penonton yang melihat video Anda, maka semakin besar pula peluang untuk meraih banyak penghasilan. Selain memudahkan dalam meraih penghasilan, subscribe juga berperan penting dalam kemungkinan mendapatkan endorsement. Hal
+                    ini sering terlihat melalui berbagai hadiah hingga <em>give away.<span class="Apple-converted-space">&nbsp;</span></em></p>
+                <p class="p2">Penting untuk Anda ketahui pula, adanya banyak subscriber tentu akan mendorong Anda untuk semakin rajin berkreasi. Hal yang menguntungkan apabila Anda memiliki subscriber banyak yaitu memperluas koneksi.</p>
+                <p class="p2">Anda berkesempatan untuk terhubung dengan pemilik konten lain. Jalinan ini bisa sekedar pertemanan sampai dengan bisnis. Bisa terlihat bahwa subscriber tak hanya memudahkan Anda dalam meraup keuntungan saja, melainkan juga menggapai kesuksesan di dunia online.</p>
+                <p class="p2">Jasaklan.com pun bisa Anda pilih untuk mendapatkan penambahan subscribe dalam waktu singkat. Adanya banyak subscriber di akun Anda, maka akan lebih meyakinkan penonton maupun subscriber lainnya untuk setia memantau YouTube maupun sosial media Anda. Tak perlu khawatir
+                    subscriber akan berkurang seiring berjalannya waktu sebab sudah permanen sesuai kesepakatan bersama.</p>
+                <p class="p2">Melalui website jasaklan.com, kami siap membantu Anda untuk memajukan dan mengembangkan bisnis online. Sebagaimana yang kita tahu bahwa di era serba modern seperti sekarang ini, dunia online seakan sudah sangat familiar.</p>
+                <p class="p2">Berbagai keperluan bisa dipenuhi secara baik hanya dengan online. Hal ini menciptakan kesan praktis, mudah, cepat, dan hemat biaya. Jasa yang kami tawarkan pun akan semakin menguntungkan Anda. Spesialisasi kami di bidang internet tak perlu diragukan.</p>
+                <p class="p2"><strong>Visi dan Misi</strong></p>
+                <p class="p2">Visi</p>
+                <ul class="ul1" style="list-style-type:none;">
+                    <li class="li2"><span class="s1">&bull; &nbsp; &nbsp;</span>Menjadi spesialis di bidang internet yang bisa diandalkan, terutama yang berkaitan dengan sosial media.</li>
+                    <li class="li2"><span class="s1">&bull; &nbsp; &nbsp;</span>Menjadi jasa di bidang internet yang terbaik dan terpercaya di seluruh Indonesis maupum mancanegara.</li>
+                    <li class="li2"><span class="s1">&bull; &nbsp; &nbsp;</span>Jasa terbaik dalam penambahan followers, subscriber, views, comment, like, share,dan lainnya.</li>
+                </ul>
+                <p class="p2">Misi</p>
+                <ul class="ul1" style="list-style-type:none;">
+                    <li class="li2"><span class="s1">&bull; &nbsp; &nbsp;</span>Membantu siapa saja yang terjun ke dunia online agar bisnisnya semakin maju dan berkembang.</li>
+                    <li class="li2"><span class="s1">&bull; &nbsp; &nbsp;</span>Memudahkan pebisnis online untuk meraup keuntungan dan penghasilan, khususnya bagi para <em>content creator.</em></li>
+                    <li class="li2"><span class="s1">&bull; &nbsp; &nbsp;</span>Mendorong orang yang bergelut di bisnis online supaya mencapai kesuksesan yang diimpikannya.</li>
+                    <li class="li2"><span class="s1">&bull; &nbsp; &nbsp;</span>Memberi peluang bagi semua orang, terutama pengguna aktif sosial media untuk memiliki penghasilan tambahan.</li>
+                </ul>
+                <p class="p2">Perlu untuk Anda sadari bahwa subscribe yang banyak memungkinkan channel Anda lebih cepat meraih popularitas. Orang-orang akan melihat akun Anda memiliki potensi besar dan bagus. Bisa dibilang bahwa banyaknya jumlah subscriber akan memberi kesan yang baik untuk akun
+                    Anda.</p>
+                <p class="p2">Siapa saja pasti akan merasa penasaran kenapa akun yang Anda miliki memiliki banyak subscriber sehingga tertarik untuk menonton konten yang diunggah. Semakin banyak orang yang memperhatikan akun Anda, maka bukan hanya subscriber saja yang meningkat, melainkan juga
+                    jumlah viewers, comment, hingga like.</p>
+                <p class="p2">Penting untuk dipahami pula bahwa jumlah viewers dan subscriber menjadi salah satu syarat yang ditetapkan YouTube dalam Google AdSense. Dengan begitu, apabila Anda memiliki banyak subscriber dan viewers, maka bisa mendapatkan penghasilan tambahan dari iklan di YouTube.
                 </p>
-
-
+                <p class="p2">Melalui visi dan misi di atas, jasaklan.com membantu Anda mendapatkan penambahan subscriber, followers, like, comment, share, dan lainnya secara instan guna memudahkan dalam merasakan keuntungan tersebut.</p>
+                <p class="p2"><strong>Alasan Memilih Kami</strong></p>
+                <p class="p2">Ada berbagai alasan mengapa Anda harus memilih kami dalam memenuhi kebutuhan di bidang jasa internet.</p>
+                <p class="p2"><strong>Profesional</strong></p>
+                <p class="p2">Kami memiliki jam terbang tinggi dan siap memberikan pelayanan secara profesional. Jasa yang kami tawarkan bisa dipertanggungjawabkan. Sikap profesional yang kami miliki juga memungkinkan kinerja cekatan dan sesuai pesanan.</p>
+                <p class="p2"><strong>Reputasi Baik</strong></p>
+                <p class="p2">Kami menyadari sepenuhnya bahwa reputasi menjadi salah satu pertimbangan dalam memilih jasa subscribe, like, comment, followers dan lainnya. Maka dari itu, kami senantiasa membangun dan menjaga reputasi agar tetap jadi yang terbaik dan terpercaya.</p>
+                <p class="p2"><strong>Mudah Dihubungi</strong></p>
+                <p class="p2">Dalam menawarkan jasa subscribe, like, comment, dan lain sebagainya, kami menyantumkan beragam kontak yang mudah dihubungi. Kebutuhan Anda terkait penambahan subscribe, like, comment, dan lainnya pun bisa terpenuhi secara cepat.</p>
+                <p class="p2"><strong>Respon Cepat dan Ramah</strong></p>
+                <p class="p2">Kontak yang mudah dihubungi memungkinkan Anda untuk mendapatkan respon secara sigap. Selain cepat, respon yang kami berikan juga ramah dan sopan.</p>
+                <p class="p2"><strong>Keamanan Data Terjamin</strong></p>
+                <p class="p2">Kami melayani jasa subscribe, like, share, comment, follower, dan lainnya dengan metode legal. Prosesnya tak membutuhkan password maupun email. Anda hanya perlu menginformasikan username yang digunakan.</p>
+                <p class="p2"><strong>Bergaransi</strong></p>
+                <p class="p2">Kami menawarkan garansi sebagai bukti tanggung jawab dan kualitas pelayanan. Apabila nantinya tidak ada penambahan subscribe, like, comment, maupun lain sebagainya, maka kami tak sungkan untuk mengembalikan uang yang telah Anda bayarkan dengan s &amp; k..</p>
+                <p class="p2"><strong>Tersedia Banyak Paket</strong></p>
+                <p class="p2">Dalam menawarkan jasa subscribe, like, comment, dan lain sebagainya, kami memberikan banyak paket yang bisa dipilih sesuai kebutuhan dan keinginan. Tiap paket yang kami sediakan memiliki layanan dan harga yang berbeda-beda.</p>
+                <p class="p2"><strong>Transaksi Mudah dan Cepat</strong></p>
+                <p class="p2">Kami jasaklan.com menerapkan teknologi verifikasi sehingga transaksi yang dilakukan bisa berlangsung secara otomatis tanpa menyita waktu dan tenaga. Di samping itu, pembayaran bisa dilakukan melalui beragam pilihan. Mulai dari transfer dari berbagai bank, aplikasi OVO,
+                    DANA, dan masih banyak lagi.</p>
+                <p class="p2"><strong>Pemesanan Mudah</strong></p>
+                <p class="p2">Pilih layanan terlebih dahulu. Setelah itu, Anda bisa pilih metode pembayarannya. Langkah selanjutnya, Anda tinggal menunggu subscribe, like, comment, ataupun followers bertambah. Biasanya penambahan tersebut paling lama 1 hari sejak pemesanan. Apabila setelah hari
+                    berikutnya masih belum ada perubahan, maka Anda bisa ajukan garansi.</p>
+                <p class="p2"><strong>Support Customer Service 24 Jam</strong></p>
+                <p class="p2">Layanan yang kami tawarkan didukung dengan customer service selama 24 jam. Bersama kami, Anda bisa jadi content creator berkualitas dan layak diperhitungkan.</p>
+                <p class="p2"><strong>Harga Terjangkau</strong></p>
+                <p class="p2">Kami memberikan harga tanpa membebani Anda. Harga jasa yang kami tawarkan relatif terjangkau sehingga bersahabat dengan dompet Anda.</p>
+                <p class="p2">Jasaklan.com menjawab semua kebutuhan Anda yang menginginkan jasa subscribe, view, like, comment, followers, share, dan lain sebagainya untuk menunjang kemajuan bisnis online. Tentu suatu keputusan yang tepat apabila Anda mempercayakan keperluan tersebut kepada kami. Di
+                    tangan ahlinya, dijamin hasilnya akan sesuai harapan.</p>
+                </p>
             </div>
         </div>
     </section>
