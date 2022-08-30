f4b24d17e386bdc8433bab2b6e7d006644466300
Yudi Candra <yudhichandra02@gmail.com>
2022-08-30 22:15:00 +0700

    payment method

--- app/Http/Controllers/CallbackController.php
+++ app/Http/Controllers/CallbackController.php
@@ -9,13 +9,13 @@ class CallbackController extends Controller
 {
     public function callback(Request $request)
     {
-        if ($request['resultCode'] == "00") {
-            $balance = Balance::where('description', "Deposit#" . $request['reference'])->first();
+        if ($request->resultCode == "00") {
+            $balance = Balance::where('description', "Deposit#" . $request->reference)->first();
             if ($balance) {
                 $balance->update(['status' => 1]);
             }
-        } else if ($request['resultCode'] == "01") {
-            $balance = Balance::where('description', "Deposit#" . $request['reference'])->first();
+        } else if ($request->resultCode == "01") {
+            $balance = Balance::where('description', "Deposit#" . $request->reference)->first();
             if ($balance) {
                 $balance->update(['status' => 3]);
             }
